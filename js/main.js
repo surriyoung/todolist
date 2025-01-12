5d5b2f0a010baaee2086c06a5fd1a8f198b9d0f7
yongjin park <70016523+gamcho3@users.noreply.github.com>
2025-01-12 09:47:50 +0900

    코드 수정

--- js/main.js
+++ js/main.js
@@ -1,168 +1,138 @@
 // 화살표 함수로 변경
 $(document).ready(() => {
+  // 시작시 리스트의 모든 항목을 불러오는 함수 호출
   getTodosListAll();
+
+  // input태그 엔터 이벤트 등록
+  let input = $("#todoInput");
+  input.keyup((event) => {
+    // 브라우저에서 입력중 상태를 감지하는 조건
+    if (event.isComposing || event.keyCode === 229) {
+      // 한글 조합중일경우 이벤트를 발생하지 않고 반환
+      return;
+    }
+    // enter 이벤트가 발생하거나 Input의 값이 존재할때 실행
+    if (event.key === "Enter" && input.val()) {
+      // addTodo 함수 호출
+      addTodo(input.val());
+      // input안의 값을 지움
+      input.val("");
+    }
+  });
 });
+
+// todos 변수를 전역으로 선언
+let todos;
+/**
+ * 투두 element 생성
+ * @param {Object} todo
+ */
+const todoComponent = (todo) => {
+  // li태그 생성
+  let list = $("<li>", {
+    class: "list",
+    id: todo.id,
+  });
+
+  // 체크박스 생성
+  let checkBox = $("<div>", {
+    class: "check",
+    id: "chk",
+    name: "ck",
+  });
+  // 체크박스 이벤트 등록
+  checkBox.click(() => checkClick(todo.id));
+
+  //콘텐츠가 들어갈 p태그 생성
+  let content = $("<p>", {
+    class: "cont",
+    text: todo.content,
+  });
+
+  // 버튼 생성
+  let button = $("<button>", {
+    type: "button",
+    class: "delete",
+    id: "delete",
+  });
+  // 버튼 이벤트 등록
+  button.click(() => deleteClick(todo.id));
+
+  // list에 각 항목을 순서대로 삽입
+  list.append(checkBox);
+  list.append(content);
+  list.append(button);
+  // 생성된 항목을 마지막 순서에 넣기
+  $(".list:last-child").after(list);
+};
 
 // 전체 todo가져오기
 function getTodosListAll() {
-  todos = JSON.parse(localStorage.getItem("todos"));
-  if (todos == null) {
-    todos = { todo: [] };
-  }
-  for (var i = 0; i < todos.todo.length; i++) {
-    if (todos.todo[i].checked == 0) {
-      console.log("???" + todos.todo[i].checked);
-      reusltHtml =
-        `
-                        <li class="list">
-                        <div class="check" id="chk" name="ck" onclick="checkClick(` +
-        todos.todo[i].id +
-        `)"></div>
-                        <p class="cont">` +
-        todos.todo[i].content +
-        `</p>
-                        <button type='button' class="delete"  id="delete" onclick="deleteClick(` +
-        todos.todo[i].id +
-        `)"><span></span><span></span></button>
-                        </li>
-                    `;
-    } else {
-      reusltHtml =
-        `
-                        <li class="list">
-                        <div class="check checked" id="chk" name="ck" onclick="checkClick(` +
-        todos.todo[i].id +
-        `)"></div>
-                        <p class="cont">` +
-        todos.todo[i].content +
-        `</p>
-                        <button type='button' class="delete"  id="delete" onclick="deleteClick(` +
-        todos.todo[i].id +
-        `)"><span></span><span></span></button>
-                        </li>
-                    `;
-    }
-    $(".list:last-child").after(reusltHtml);
-  }
+  // 로컬스토리지에 todos의 데이터를 가져오고 없으면 빈배열 할당
+  todos = JSON.parse(localStorage.getItem("todos")) ?? [];
+
+  // ul태그 선택
+  let ul = $(".todo_list");
+  // ul태그 안의 내용 전부 비우기
+  ul.empty();
+  // ul태그에 빈 li태그 넣기
+  ul.append(
+    $("<li>", {
+      class: "list",
+    })
+  );
+  // todos의 길이만큼 반복
+  todos.forEach((item) => todoComponent(item));
 }
 
-let todos = {};
-let checkBox;
-
 function addTodo(txt) {
-  let todos = JSON.parse(localStorage.getItem("todos")); //데이터 불러오기
+  console.log(txt);
 
-  //시간으로 추가
+  // 새로운 todo 항목 생성
   let todo = {
     id: Date.now(), // 현재시간 기준으로 저장
-    checked: 0, // 0: false , 1: true
+    checked: false, // 0: false , 1: true
     content: txt,
   };
 
-  if (todos == null) {
-    //todolist가 없을때
-    todos = { todo: [] }; //todos.todo[0].id
-    todos.todo.push(todo);
-    localStorage.setItem("todos", JSON.stringify(todos));
-  } else {
-    //todo.id = todos.todo.length;
-    todos.todo.push(todo);
-    localStorage.setItem("todos", JSON.stringify(todos));
-  }
+  // todos에 새로만든 todo 저장
+  todos.push(todo);
+  // todos를 로컬스토리지에 저장
+  localStorage.setItem("todos", JSON.stringify(todos));
 
-  //글 한개 추가
-  var reusltHtml =
-    `
-                    <li class="list">
-                    <div class="check" id="chk" name="ck" onClick="checkClick(` +
-    todo.id +
-    `)"></div>
-                    <p class="cont">` +
-    txt +
-    `</p>
-                    <button type='button' class="delete"  id="delete" onclick="deleteClick(` +
-    todo.id +
-    `)"><span></span><span></span></button>
-                    </li>
-                `;
-  $(".list:last-child").after(reusltHtml);
+  //글 불러오기
+  getTodosListAll();
 }
 
-function enterClick() {
-  var code = window.event.keyCode;
-  txt = document.getElementById("todoInput").value;
-
-  if (code == 13 && txt != "") {
-    addTodo(txt);
-  }
-}
 function addBtnClick() {
-  txt = document.getElementById("todoInput").value;
-  console.log("텍스트 입력값 : ", txt);
-  if (txt != "") {
-    addTodo(txt);
-  }
+  console.log("call addBtnClick");
+  let input = $("#todoInput");
+  console.log("텍스트 입력값 : ", input.val());
+  input.val() && addTodo(input.val());
+  input.val("");
 }
 
 function deleteClick(num) {
-  let todos = JSON.parse(localStorage.getItem("todos"));
-  let lastNum = todos.todo.length;
-  if (num + 1 == lastNum) {
-    deleteTodoLastOne();
-    console.log("마지막투두삭제");
-  } else {
-    deleteTodoById(num); // 선택된 todo 삭제
-    console.log("마지막아닌거 삭제" + num);
-  }
-
-  //location.reload(); // 다른방법 뭐지.. 답변) 1.전체 dom 삭제 후 전체 리스트를  다시 그린다 ( 삭제가 완료되었을때)
-  // 2. 해당하는 dom만삭제를 한다.  dom선택자로 id값에 해당하는 dom을 찾아서 remove
-  $(".list:not(:first-child)").remove();
-  getTodosListAll();
+  deleteTodoById(num); // 선택된 todo 삭제
+  $(`#${num}`).remove(); // 선택된 id에 해당하는 태그 삭제
+  getTodosListAll(); // 리스트 불러오기
 }
+
 function deleteTodoById(selectedId) {
-  // 선택된 아이디
-  let todos = JSON.parse(localStorage.getItem("todos")); //데이터 불러오기
-  // searchId = -1;
+  // 선택된 id를 제외한 모든 요소를 새로운 배열로 반환
+  let newTodo = todos.filter((item) => item.id !== selectedId);
 
-  for (let i = 0; i < todos.todo.length; i++) {
-    // 전체 Todos 반복 돌면서
-    if (selectedId === todos.todo[i].id) {
-      // 찾으려는 아이디와 같다면 삭제
-      todos.todo.splice(i, 1); // 1개 항목을 삭제
-    }
-  }
-  localStorage.setItem("todos", JSON.stringify(todos)); //데이터 불러오기
-}
-function deleteTodoLastOne() {
-  let todos = JSON.parse(localStorage.getItem("todos")); //데이터 불러오기
-  todos.todo.splice(todos.todo.length - 1, 1);
-
-  localStorage.setItem("todos", JSON.stringify(todos));
+  // 반환된 배열을 로컬스토리지에 저장
+  localStorage.setItem("todos", JSON.stringify(newTodo));
 }
 
 function checkClick(selectedId) {
-  let todos = JSON.parse(localStorage.getItem("todos"));
   console.log("체크클릭");
+  let checkedTodo = todos.find((item) => item.id === selectedId);
+  checkedTodo.checked = !checkedTodo.checked;
+  todos.sort((a, b) => a - b);
 
-  //=========================수정 ======================
-  //선택된 todo를 찾기위해 전체 todos를 반복한다.
-  for (let i = 0; i < todos.todo.length; i++) {
-    if (todos.todo[i].id === selectedId) {
-      console.log("클릭한투두찾기" + todos.todo[i].checked);
-      if (todos.todo[i].checked == 0) {
-        todos.todo[i].checked = 1;
-        console.log("체크하기" + todos.todo[i].checked);
-      } else {
-        todos.todo[i].checked = 0;
-        console.log("체크해제" + todos.todo[i].checked);
-      }
-      localStorage.setItem("todos", JSON.stringify(todos));
-
-      break; // 해당하는 값 찾았으니 반복문 종료
-    }
-  }
-
+  localStorage.setItem("todos", JSON.stringify(todos));
   $(".list:not(:first-child)").remove();
   getTodosListAll();
 }
